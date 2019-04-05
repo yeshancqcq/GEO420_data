efdb6b6de7eb7eea9d3f0723ad0a850f20fb603b
Shan Ye <yeshan@umich.edu>
2019-04-05 12:25:58 -0500

    Create main.R

---
+++ main.R
@@ -0,0 +1,23 @@
+install.packages("rjson")
+install.packages("jsonlite")
+install.packages("R.utils")
+library(rjson)
+
+# Set up the working directory
+setwd("/Users/apple/Desktop/GEO420_data") 
+# Change to your working directory: the folder where the json file and scripts are saved
+
+# Read the json file
+glacier_raw_data <- fromJSON(file = "chile_ar_ar.bibjson")
+
+# Sourcing self-developped functions in the same directory
+# This step have to be done AFTER reading the json data, because the jsonToDF.R has a package that can overwrite some functions of rjson
+source("jsonToDF.R")
+
+# Using the jsonToDf function to convert json to data frame
+glacier_data <- jsonToDf(glacier_raw_data)
+
+# Writing a csv file (optional)
+write.csv(glacier_data, "glacier_data.csv")
+
+
