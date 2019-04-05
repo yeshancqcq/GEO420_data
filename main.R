4b17e4201bd75c5b11c07fb616a1a4fb1900d26e
Shan Ye <yeshan@umich.edu>
2019-04-05 12:43:50 -0500

    Update main.R

--- main.R
+++ main.R
@@ -11,7 +11,8 @@ setwd("/Users/apple/Desktop/GEO420_data")
 glacier_raw_data <- fromJSON(file = "chile_ar_ar.bibjson")
 
 # Sourcing self-developped functions in the same directory
-# This step have to be done AFTER reading the json data, because the jsonToDF.R has a package that can overwrite some functions of rjson
+# This step have to be done AFTER reading the json data
+# because the jsonToDf.R has a package that can overwrite some functions of rjson
 source("jsonToDf.R")
 
 # Using the jsonToDf function to convert json to data frame
