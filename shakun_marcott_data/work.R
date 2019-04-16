06aab087addb77a73efbaeec258e829c8a39c414
Shan Ye <yeshan@umich.edu>
2019-04-16 16:53:56 -0500

    Create work.R

---
+++ shakun_marcott_data/work.R
@@ -0,0 +1,17 @@
+library(readr)
+library(dplyr)
+library(stringr)
+library(purrr)
+library(assertthat)
+
+getwd()
+
+source("clear_marcott_data.R")
+source("clear_shakun_data.R")
+
+A7 <- read_csv("marcott_data/A7.csv")
+View(A7)
+
+A7_cleared <- clear_marcott_data(A7)
+
+# This is just an example. Import selected dataset based on https://yeshancqcq.github.io/paleo/paleo_index.html.
