ffe620583b21fe335cc9c6edbf88052d2eec7ff7
Shan Ye <yeshan@umich.edu>
2019-04-16 17:02:22 -0500

    Update work.R

--- shakun_marcott_data/work.R
+++ shakun_marcott_data/work.R
@@ -9,7 +9,7 @@ getwd()
 source("clear_marcott_data.R")
 source("clear_shakun_data.R")
 
-A7 <- read_csv("marcott_data/A7.csv")
+A7 <- read_csv("~/marcott_data/A7.csv")
 View(A7)
 
 A7_cleared <- clear_marcott_data(A7)
